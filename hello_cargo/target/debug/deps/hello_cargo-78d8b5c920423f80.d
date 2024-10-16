7308cf7ecbbd02434631d6390550a2239d4f7e9e
Ziga Medved <47921749+zigamedved@users.noreply.github.com>
2024-10-16 22:25:21 +0200

    add cargo project

---
+++ hello_cargo/target/debug/deps/hello_cargo-78d8b5c920423f80.d
@@ -0,0 +1,5 @@
+/Users/zigamedved/Documents/GitHub/rust-projects/hello_cargo/target/debug/deps/libhello_cargo-78d8b5c920423f80.rmeta: src/main.rs
+
+/Users/zigamedved/Documents/GitHub/rust-projects/hello_cargo/target/debug/deps/hello_cargo-78d8b5c920423f80.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/zigamedved/Documents/GitHub/rust-projects/hello_cargo/target/release/hello_cargo: /Users/zigamedved/Documents/GitHub/rust-projects/hello_cargo/src/main.rs
